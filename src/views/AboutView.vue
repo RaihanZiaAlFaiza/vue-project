a23e8691f06afc8bd7987e18005c88754034fdda
RaihanZiaAlFaiza <129245944+RaihanZiaAlFaiza@users.noreply.github.com>
2023-05-17 10:50:48 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="about">
-    <h1>HELLO</h1>
-  </div>
+  <nav>
+    <div class="about">
+      <h1>Sejarah singkat Al-Quran</h1>
+    </div>
+    <div class="image-quran-center">
+      <img src="../assets/quran.jpg" />
+    </div>
+    <div class="text">
+      <p>
+        Sahabat mari kita mengenal sedikit tentang Sejarah Al Quran. Sebagai umat Islam kita pasti sudah tahu bahwa Al-Qur’an (القرآن) merupakan kallam Allah Subhanahu wa ta’ala yang memiliki keistimewaan dari pada kitab-kitab lainnya. Tapi
+        sudahkah kita mengetahui bagaimana sejarah diturunkannya Al Quran? Kurang rasanya jika kita hanya sekedar membaca dan menghafal saja tanpa mengetahui sejarah Al Quran. Syaikh Muhammad bin Shalih al-Utsaimin menjelaskan bahwa
+        Al-Qur’an adalah bentuk kata benda infinitif (mashdar) dari kata qara`a (قرأ) yang bermakna membaca atau mengumpulkan. Sedangkan, menurut terminologi Al-Qur’an adalah kitab suci yang berisi firman atau wahyu Allah ta’ala yang
+        diturunkan kepada Nabi Muhammad Shallallahu alaihi wasallam sebagai mukjizat melalui perantara malaikat Jibril. Al-Qur’an terdiri atas 114 surat dan dibagi menjadi 30 bagian atau disebut juz. Jumlah seluruh ayatnya ada 6.666 buah.
+        Allah Subhanahu wa ta’ala menurunkan Al-Qur’an sebagai satu mukjizat yang membuktikan kerasulan Nabi Muhammad, dan keberadaan Allah SWT dengan segala sifat-sifat kesempurnaanNya. Membaca Al-Qur’an dan menghayati dan mengamalkannya
+        adalah satu ibadah. Ia merupakan satu kitab panduan hidup manusia dan referensi utama umat Islam di samping sunnah Rasulullah.
+      </p>
+    </div>
+  </nav>
 </template>
+<style>
+.image-quran-center {
+  text-align: center;
+}
+</style>
 
